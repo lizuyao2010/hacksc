5cb4177de313f1927354d31446612b6a53e95822
lizuyao2010 <zuyaoli2010@gmail.com>
2015-11-14 13:49:03 -0800

    remove many to many

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,10 +11,7 @@ class Student(models.Model):
     )
     gender = models.CharField(max_length = 1, choices = GENDER,default=True)
     YEAR_IN_SCHOOL_CHOICES = (
-        ('FR', 'Freshman'),
-        ('SO', 'Sophomore'),
-        ('JR', 'Junior'),
-        ('SR', 'Senior'),
+        ('UN','UnderGraduate')
         ('GR', 'Graduate'),
     )
     year_in_school = models.CharField(max_length = 2, choices = YEAR_IN_SCHOOL_CHOICES,default=True)
@@ -23,22 +20,22 @@ class Student(models.Model):
 class Course(models.Model):
     course_id=models.CharField(max_length=30,default=True)
     course_name=models.CharField(max_length=30,default=True)
-    course_section=models.CharField(max_length=30,default=True)
-    COURSE_TYPE=(
-        ('Le','Lecture'),
-        ('La','Lab'),
-        ('Q','Quiz'),   
-    )
-    course_type=models.CharField(max_length=2,choices=COURSE_TYPE,default=True)
-    instructor=models.CharField(max_length=30,default=True)
-    location=models.CharField(max_length=30,default=True)
-    capcity=models.IntegerField(default=True)
-    registered=models.IntegerField(default=True)
-    start_time=models.TimeField(default=True)
-    end_time=models.TimeField(default=True)
+    # course_section=models.CharField(max_length=30,default=True)
+    # COURSE_TYPE=(
+        # ('Le','Lecture'),
+        # ('La','Lab'),
+        # ('Q','Quiz'),   
+    # )
+    # course_type=models.CharField(max_length=2,choices=COURSE_TYPE,default=True)
+    # instructor=models.CharField(max_length=30,default=True)
+    # location=models.CharField(max_length=30,default=True)
+    # capcity=models.IntegerField(default=True)
+    # registered=models.IntegerField(default=True)
+    # start_time=models.TimeField(default=True)
+    # end_time=models.TimeField(default=True)
     # how to add multiple choices?
-    days=models.CharField(max_length=30,default=True)
-    credits=models.FloatField(default=True)
-    students=models.ManyToManyField(Student)
+    # days=models.CharField(max_length=30,default=True)
+    # credits=models.FloatField(default=True)
+    # students=models.ManyToManyField(Student)
 
 
