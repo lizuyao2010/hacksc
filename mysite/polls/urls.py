366b2953f55e6a045c251b789c36c131797c9057
liangqiaobuaa <buaa.beijing@gmail.com>
2015-11-14 16:31:47 -0800

    sdf

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^lexusadduser/$', views.lexusadduser, name='lexusadduser'),
-    url(r'^stuInfo/$', views.stuInfo, name='stuInfo'),
-    url(r'^couInfo/$', views.couInfo, name='couInfo'),
+    url(r'^$', views.lexusadduser, name='lexusadduser'),
+    url(r'^stuInfo/(?P<student_id>[0-9a-z]+)/$', views.stuInfo, name='stuInfo'),
+    url(r'^couInfo/(?P<course_id>[0-9a-z]+)/$', views.couInfo, name='couInfo'),
 ]
