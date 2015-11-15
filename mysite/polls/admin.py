42bff0f3802a299d0f8dfd68674d1ac085650716
lizuyao2010 <zuyaoli2010@gmail.com>
2015-11-14 21:48:06 -0800

    add models to admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student,Course
+from .models import Student,Course,Student_Course,Question,Answer
 
 admin.site.register(Student)
 admin.site.register(Course)
+admin.site.register(Student_Course)
+admin.site.register(Question)
+admin.site.register(Answer)
