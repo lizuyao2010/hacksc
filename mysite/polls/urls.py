f83832325812dd087a5d1df75d9ac57875bb6335
Yuyang Zhang <zhangyy1020@gmail.com>
2015-11-14 22:16:57 -0800

    111

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,9 +3,13 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
+    url(r'^my_login/$', views.my_login, name='my_login`'),
     url(r'^adduser/$', views.adduser, name='adduser'),
+    url(r'^addStudent/$', views.addStudent, name='addStudent'),
     url(r'^main/$', views.main, name='main'),
     url(r'^stuInfo/(?P<student_id>[0-9a-z]+)/$', views.stuInfo, name='stuInfo'),
     url(r'^couInfo/(?P<course_id>[0-9a-z]+)/$', views.couInfo, name='couInfo'),
     url(r'^addQuestion/$', views.addQuestion, name='addQuestion'),
+    url(r'^questionList/(?P<course_id>[0-9a-z]+)/$', views.questionList, name='questionList'),
+    url(r'^answerList/(?P<question_id>[0-9a-z]+)/$', views.answerList, name='answerList')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import Http404
 
 # Create your views here.
@@ -7,8 +7,11 @@ from .models import Course
 from .models import Student_Course
 from .models import Question
 from .models import Answer
-from forms import *
+from forms import QuestionForm
 from django.contrib.auth import login
+from forms import UserForm
+from forms import StudentForm
+from django.contrib.auth import authenticate,login
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 
@@ -16,39 +19,72 @@ import datetime
 def main(request):
     return render(request,'polls/main.html',{})
 
+def my_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # Redirect to a success page.
+                return HttpResponseRedirect('/polls/main/')
+            else:
+                # Return a 'disabled account' error message
+                pass
+        else:
+            # Return an 'invalid login' error message.
+            pass
+    else:
+        pass
+    return render(request, 'polls/login.html', {})
+
+def addStudent(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            new_student = Student(**form.cleaned_data)
+            new_student.save()
+            # redirect, or however you want to get to the main view
+            return HttpResponseRedirect('/polls/main/')
+    else:
+        form = StudentForm() 
+
+    return render(request, 'polls/addStudent.html', {'form': form})
+
 def adduser(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
             new_user = User.objects.create_user(**form.cleaned_data)
-            # login(request,new_user)
             new_user.save()
+            # login(request,new_user)
             # redirect, or however you want to get to the main view
-            return HttpResponseRedirect('/polls/stuInfo/')
+            return HttpResponseRedirect('/polls/addStudent/')
     else:
         form = UserForm() 
 
     return render(request, 'polls/adduser.html', {'form': form}) 
 
 def stuInfo(request, student_id):
-    stu_cou = get_object_or_404(Student_Course, pk=student_id)
-    stu = get_object_or_404(Student, pk=student_id)
+    stu_cou = get_list_or_404(Student_Course, student_id=student_id)
+    stu = get_object_or_404(Student, student_id=student_id)
+
     cou_id_list = []
     for s_c in stu_cou:
         cou_id_list.append(s_c.course_id)
 
     course_list = []
     for cou_id in cou_id_list:
-        course = get_object_or_404(Course, pk=cou_id)
+        course = get_object_or_404(Course, course_id=cou_id)
         course_list.append(course)
 
     context = {'course_list': course_list, 'student_info': stu}
     return render(request, 'polls/stuInfo.html', context)
 
 def couInfo(request, course_id):
-
-    stu_cou = get_object_or_404(Student_Course, pk=course_id)
-    cou = get_object_or_404(Course, pk=course_id)
+    stu_cou = get_list_or_404(Student_Course, course_id=course_id)
+    cou = get_object_or_404(Course, course_id=course_id)
 
     stu_id_list = []
     for s_c in stu_cou:
@@ -56,7 +92,7 @@ def couInfo(request, course_id):
 
     student_list = []
     for stu_id in stu_id_list:
-        student = get_object_or_404(Student, pk=stu_id)
+        student = get_object_or_404(Student, student_id=stu_id)
         student_list.append(student)
     context = {'student_list': student_list, 'cou_info': cou}
     return render(request, 'polls/couInfo.html', context)
@@ -73,3 +109,13 @@ def addQuestion(request):
         form = QuestionForm() 
 
     return render(request, 'polls/addQuestion.html', {'form': form}) 
+def questionList(request, course_id):
+    question_list = get_list_or_404(Question, course_id = course_id)
+    context = {'question_list': question_list}
+    return render(request, 'polls/questionList.html', context)
+
+def answerList(request, question_id):
+    answer_list = get_list_or_404(Answer, question_id = question_id)
+    question = get_object_or_404(Question, question_id=question_id)
+    context = {'question': question, 'answer_list': answer_list}
+    return render(request, 'polls/answerList.html', context)
